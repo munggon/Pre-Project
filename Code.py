eb844ef5a185754585c7e959064019547354e85a
munggon <dragonlnw@msn.com>
2022-02-27 22:44:42 +0700

    hello world

--- Code.py
+++ Code.py
@@ -1,8 +1,7 @@
 import pandas as pd;
-df = pd.read_csv("./Data/PROBE-201901/20190101.csv.out", sep = ',' , names = ['VehicleID','gpsvalid','lat','lon','timestamp','speed','heading','for_hire_light','engine_acc'])
+df = pd.read_csv("./Data/PROBE-201902/20190203.csv.out", sep = ',' , names = ['VehicleID','gpsvalid','lat','lon','timestamp','speed','heading','for_hire_light','engine_acc'])
 
-#a = ['00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23']
-
+hours = ['00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23']
 split_date = df['timestamp'].str.split(' ', expand = True)
 
 df['date'] = split_date[0]
@@ -10,13 +9,11 @@ df['timeR'] = split_date[1]
 
 #print(df.loc[(df['date'] == '2019-01-01') & (df['time'] == '00:00:00')])
 
-#for i in a :
-#    aq = df[df['timeR'].str.contains('^'+i)]
-#    aaq = aq.loc[(df['date'] == '2019-01-01')]
-#    aaq.to_csv("./Data/Hour/"+i+"-20190101.csv.out" ,index = False)
+for i in hours :
+    aq = df[df['timeR'].str.contains('^'+i)]
+    aaq = aq.loc[(df['date'] == '2019-02-03')]
+    aaq.to_csv("./Data/201902/201902-03/"+i+"-20190203.csv.out" ,index = False)
 
-aq = df[df['timeR'].str.contains('^02')]
-aaq = aq.loc[(df['date'] == '2019-01-01')]
-aaq.to_csv("./Data/Hour/02-20190101.csv.out" ,index = False)
+print (aaq) 
 
 
